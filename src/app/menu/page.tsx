06ba24623b5db78792d00f5e06264924ae33f065
mu_francism <francismiguelgarcia72@gmail.com>
2025-08-22 11:28:33 +0800

    added basic routes

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link';
+
+const Menu = () => {
+    const NCPAG_LINK = 'https://ncpag.upd.edu.ph/'
+    const GRIT_LINK = 'https://www.facebook.com/UPNCPAGGRITLabs'
+
+    return (
+    <>
+        <div className="my-6 flex justify-center items-center">
+            <Link href={ NCPAG_LINK } passHref>
+                <Image
+                    src="/assets/ncpag-logo.png"
+                    width={75}
+                    height={75}
+                    className='m-1'
+                    alt=""
+                />
+            </Link>
+            <Link href={ GRIT_LINK } passHref>
+                <Image
+                    src="/assets/grit-logo-round.png"
+                    width={80}
+                    height={80}
+                    className='m-1'
+                    alt=""
+                />
+            </Link>
+        </div>
+
+        <h1 className="text-2xl text-center my-2">Multi-Criteria Decision Analysis</h1>
+        <div className="text-xl text-center my-2">Analytic Hierarchical Process</div>
+
+        <div className="text-center my-6 block">
+            <Link href='/create-room' passHref>
+                <button type="button" id="create-room" className="w-xs text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Decision Room</button>
+            </Link>
+            <Link href='/join-room' passHref>
+                <button type="button" id="join-room" className="w-xs focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Join Room</button>
+            </Link>
+        </div>
+    </>
+    )
+}
+
+export default Menu
